f972b9529498c6ba342b9d79af873900298f4851
LTKThoa <thoaltk.23itb@vku.udn.vn>
2024-01-22 23:07:00 +0700

    QLThueBao

---
+++ BTL_ck1/ck/src/controller/DOANH_THU.java
@@ -0,0 +1,27 @@
+//package controller;
+//
+//public class DOANH_THU extends THU_CUOC{
+//	double doanhthu_thang;
+//	double doanhthu_nam;
+//	public DOANH_THU(int id, String ngaythanhtoan, String tinhtrangTB, String loaiTB, String ten, String sdt, double phi_cuoc, String goi_dk, String ngaydk,
+//		boolean thanh_toan, double doanhthu_thang, double doanhthu_nam) {
+//		super(id, ngaythanhtoan, ten, sdt,loaiTB, tinhtrangTB,  );
+//		this.doanhthu_thang = doanhthu_thang;
+//		this.doanhthu_nam = doanhthu_nam;
+//	}
+//	public double getDoanhthu_thang() {
+//		return doanhthu_thang;
+//	}
+//	public void setDoanhthu_thang(double doanhthu_thang) {
+//		this.doanhthu_thang = doanhthu_thang;
+//	}
+//	public double getDoanhthu_nam() {
+//		return doanhthu_nam;
+//	}
+//	public void setDoanhthu_nam(double doanhthu_nam) {
+//		this.doanhthu_nam = doanhthu_nam;
+//	}
+//	
+//
+//
+//}
---
+++ BTL_ck1/ck/src/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+import login.view_login;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new view_login();
+	}
+
+}
